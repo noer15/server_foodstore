8729c3534c42f22532c74c39740b2c9a83081aed
Nur Hidayat <noer4236@gmail.com>
2022-04-07 15:17:43 +0700

    update api

--- db/index.js
+++ db/index.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 const { dbHost, dbName, dbPort, dbUser, dbPass } = require("../app/config");
 
+// mongoose.connect(
+//   `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
+  `mongodb+srv://${dbUser}:${dbPass}@${dbHost}/${dbName}?authSource=admin&replicaSet=atlas-ap8r99-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
